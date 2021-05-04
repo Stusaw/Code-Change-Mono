e31d019b0d2b2a928ba69e71b66deb4bf97a330d
Gregg Sawyer <gregg@codechange.co.uk>
2021-05-04 12:08:00 +0100

    initial commit

---
+++ apps/dealership-portal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'code-change-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dealership-portal';
+}
